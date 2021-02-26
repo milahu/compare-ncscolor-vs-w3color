003071605d2cc20aea4bcf4ca5810a8eaffd3346
milahu <milahu@gmail.com>
2021-02-26 15:17:06 +0100

    add: detect unsteady diff

--- test-ncscolor.js
+++ test-ncscolor.js
@@ -24,6 +24,7 @@ function ncsOfDegrees(d) {
 // ncs format: S ${nuance.s}${nuance.c}-${hue.a}${hue.phi}${hue.b}
 
 const log_invalid = false; // 1 <= hue.phi <= 9: invalid color for some algorithms
+const log_mismatch = false; // show mismatches between libraries
 
 const maxval = {
   w3color: {
@@ -46,16 +47,18 @@ const maxval = {
 //   unsteady value: ncsColor.col.r: 253 - 357 = -41%
 //   unsteady value: ncsColor.col.g: 255 - 357 = -40%
 //   unsteady value: ncsColor.col.b: 36 - 51 = -6%
+// here we can select the keys for testing
 const tolerance = {
+  /*
   w3color: {
     col: {
-      // TODO minimize
       red: 6, green: 5, blue: 6,
       hue: 7,
       sat: 2, lightness: 12,
       whiteness: 22, blackness: 7,
     },
   },
+  */
   ncsColor: {
     col: {
       r: 6, g: 5, b: 5,
@@ -74,6 +77,10 @@ function fmtRgb(val) {
   return 'rgb(?,?,?)';
 }
 
+function fmtDiff(diff_100) {
+  return (diff_100 > 0 ? '+' : '') + diff_100 + '%';
+}
+
 
 
 let errors_found = false;
@@ -89,6 +96,8 @@ let last = {
 for (let colorDegreesFull = 0; colorDegreesFull <= 420; colorDegreesFull++) {
   const colorDegrees = colorDegreesFull % 400;
   const ncs = '0580-' + ncsOfDegrees(colorDegrees);
+
+  //console.log(`degree ${colorDegrees} / 400 = ncs ${ncs}`);
 
   let log_added = false;
 
@@ -118,10 +127,11 @@ for (let colorDegreesFull = 0; colorDegreesFull <= 420; colorDegreesFull++) {
     curr.ncsColor.col = { r, g, b };
 
     if (
+      log_mismatch && (
       curr.ncsColor.col.r != curr.w3color.col.red ||
       curr.ncsColor.col.g != curr.w3color.col.green ||
       curr.ncsColor.col.b != curr.w3color.col.blue
-    ) {
+    )) {
       console.log(`mismatch: degree ${curr.colorDegrees} / 400 = ncs ${ncs}:`);
       console.log(`            last             -> curr (mismatch)`);
       console.log(`  w3color:  ${fmtRgb(last.w3color)} -> ${fmtRgb(curr.w3color)}`);
@@ -130,6 +140,11 @@ for (let colorDegreesFull = 0; colorDegreesFull <= 420; colorDegreesFull++) {
     }
   }
 
+  // debug
+  //if (270 < colorDegrees && colorDegrees < 280) console.dir({ curr }, { depth: null });
+
+  const diff = {};
+
   // test steadiness of values
   Object.keys(tolerance).forEach(key1 => {
     if (last[key1] == null || curr[key1] == null) return;
@@ -137,8 +152,12 @@ for (let colorDegreesFull = 0; colorDegreesFull <= 420; colorDegreesFull++) {
     const d1 = last[key1].colorDegrees;
     const d2 = curr[key1].colorDegrees;
     let deg_diff = d2 - d1;
+    (key1 in diff) || (diff[key1] = {});
+    diff[key1].colorDegrees = { d1, d2 };
+    diff[key1].ncs = { n1: last[key1].ncs, n2: curr[key1].ncs };
     Object.keys(tolerance[key1]).forEach(key2 => {
       if (last[key1][key2] == null || curr[key1][key2] == null) return;
+      (key2 in diff[key1]) || (diff[key1][key2] = {});
       Object.keys(tolerance[key1][key2]).forEach(key3 => {
         const v1 = last[key1][key2][key3];
         const v2 = curr[key1][key2][key3];
@@ -148,20 +167,66 @@ for (let colorDegreesFull = 0; colorDegreesFull <= 420; colorDegreesFull++) {
         const tol_scale = (deg_diff - 1)*(1 + upscale_tolerance) + 1;
         const max = maxval[key1][key2][key3];
         const tol_100 = tolerance[key1][key2][key3];
-        let diff = v2 - v1;
-        if (key2 == 'col' && key3 == 'hue' && (diff < -300 || 300 < diff)) {
-          diff = diff - Math.sign(diff)*360; // modulo
+        let diff_curr = v2 - v1;
+        if (key2 == 'col' && key3 == 'hue' && (diff_curr < -300 || 300 < diff_curr)) {
+          diff_curr = diff_curr - Math.sign(diff_curr)*360; // modulo
         }
-        const diff_100 = Math.round(diff/max * 100);
+        const diff_100 = Math.round(diff_curr/max * 100);
+        const diff_100_scaled = diff_100 / tol_scale;
+        diff[key1][key2][key3] = diff_100_scaled;
         const diff_100_abs = Math.abs(diff_100);
+        const diff_100_abs_scaled = diff_100_abs / tol_scale;
 
-        if (diff_100_abs > (tol_100 * tol_scale)) {
-          console.log(`unsteady: degree ${d1} -> ${d2} / 400 = ncs ${last[key1].ncs} -> ${curr[key1].ncs}:`);
+        // debug
+        /*
+        if (270 < colorDegrees && colorDegrees < 280 && (key3 == 'red' || key3 == 'green' || key3 == 'blue')) {
+          console.log(`test: degree ${d1} -> ${d2} / 400 = ncs ${last[key1].ncs} -> ${curr[key1].ncs}:`);
           console.log(`  last: ${fmtRgb(last[key1])}. ${key1}.${key2}.${key3} = ${v1}`);
           console.log(`  curr: ${fmtRgb(curr[key1])}. ${key1}.${key2}.${key3} = ${v2} = ${(diff_100 > 0 ? `+${diff_100}` : diff_100)}%`);
+        }
+        */
+        if (diff_100_abs_scaled > tol_100) {
+          console.log(`unsteady value in library ${key1}:`);
+          console.log(`  last: ${d1}/400 = ncs ${last[key1].ncs}: ${fmtRgb(last[key1])}. ${key1}.${key2}.${key3} = ${v1}`);
+          console.log(`  curr: ${d2}/400 = ncs ${curr[key1].ncs}: ${fmtRgb(curr[key1])}. ${key1}.${key2}.${key3} = ${v2} = diff ${fmtDiff(diff_100)} (max ${tol_100}%)`);
           log_added = true;
           errors_found = true;
         }
+
+
+        const tol_dd_scale = 1; // diff_diff tolerance scale factor
+
+        // compare diff to last.diff
+        if (last.diff && last.diff[key1] && last.diff[key1][key2] && last.diff[key1][key2][key3]) {
+          //console.dir({ last_diff: last.diff }, { depth: null });
+          const diff1 = last.diff[key1][key2][key3];
+          const diff2 = diff[key1][key2][key3];
+          const diff_diff = diff2 - diff1;
+          const diff_diff_abs = Math.abs(diff_diff);
+
+          /*
+          // debug
+          //if (270 < colorDegrees && colorDegrees < 280 && (key3 == 'red' || key3 == 'green' || key3 == 'blue')) {
+          if (270 < colorDegrees && colorDegrees < 280 && (key3 == 'blue')) {
+            console.log(`test diff_diff: degree ${d1} -> ${d2} / 400 = ncs ${last[key1].ncs} -> ${curr[key1].ncs}:`);
+            console.log(`  last: ${key1}.${key2}.${key3} -= ${fmtDiff(last.diff[key1][key2][key3])}`);
+            console.log(`  curr: ${key1}.${key2}.${key3} -= ${fmtDiff(diff[key1][key2][key3])}`);
+            console.log(`  diff_diff: ${diff_diff}`);
+            console.log(`  max_ddiff: ${(tol_100 * tol_scale * tol_dd_scale)}`);
+          }
+          */
+
+
+          if (diff_diff_abs > tol_100) {
+            //diff[key1].colorDegrees = { d1, d2 };
+            console.log(`unsteady diff in library ${key1}:`);
+            console.log(`  last: ${last.diff[key1].colorDegrees.d1} -> ${last.diff[key1].colorDegrees.d2} /400 = ${last.diff[key1].ncs.n1} -> ${last.diff[key1].ncs.n2}: ${key1}.${key2}.${key3} -= ${fmtDiff(last.diff[key1][key2][key3])}`);
+            console.log(`  curr: ${d1} -> ${d2} /400 = ${last[key1].ncs} -> ${curr[key1].ncs}: ${key1}.${key2}.${key3} -= ${fmtDiff(diff[key1][key2][key3])}' -> diff ${fmtDiff(diff_diff)} (max ${tol_100}%)`);
+            log_added = true;
+            errors_found = true;
+          }
+        }
+
       });
     });
   });
@@ -187,6 +252,7 @@ for (let colorDegreesFull = 0; colorDegreesFull <= 420; colorDegreesFull++) {
       ncsColor: last.ncsColor,
     };
   }
+  last.diff = diff;
 }
 
 if (errors_found) {
